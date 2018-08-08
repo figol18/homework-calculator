9c64c6a2879045a1c039c58f0c9e6ba267b2cb15
Andrew <figol_st@ukr.net>
2018-08-08 13:26:53 +0000

    calculator pat 1

---
+++ js/index.js
@@ -0,0 +1,31 @@
+const display = document.querySelector('.calculator .display');
+
+document.querySelectorAll('.numbers button')
+  .forEach( btn => btn.addEventListener('click', numClick));
+
+let operator = document.querySelectorAll('.operators button');
+operator.forEach( btn => btn.addEventListener('click', operClick));
+
+function numClick(event) {
+    display.value += event.target.innerText;
+  event.preventDefault();
+}
+
+function operClick(event) {
+  if(display.value.slice(-1) == '+' || display.value.slice(-1) == '-' || display.value.slice(-1) == '*' || display.value.slice(-1) == '/') {
+        display.value = display.value.replace(/\D/g, event.target.innerText);
+  } else {
+     display.value += event.target.innerText;
+  }
+  event.preventDefault();
+}
+
+document.querySelector('.equal').addEventListener('click', function (event) {
+  display.value = eval(display.value);
+  event.preventDefault();
+});
+
+document.querySelector('.clear').addEventListener('click', function (event) {
+  display.value = '';
+  event.preventDefault();
+});
